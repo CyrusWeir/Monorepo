a2ff9c04684d38d8c05135eecb8e4006575a7e9b
CyrusWeir <cyrusweir@gmail.com>
2025-01-27 10:25:07 +0100

    ajouté program Repository avec une fonction browse dans programActions.

---
+++ server/src/modules/program/programRepository.ts
@@ -0,0 +1,21 @@
+import databaseClient from "../../../database/client";
+
+import type { Result, Rows } from "../../../database/client";
+
+type Program = {
+  id: number;
+  title: string;
+  category_id: number;
+};
+
+class ProgramRepository {
+  async readAll() {
+    // Execute the SQL SELECT query to retrieve all programs from the "program" table
+    const [rows] = await databaseClient.query<Rows>("select * from program");
+
+    // Return the array of programs
+    return rows as Program[];
+  }
+}
+
+export default new ProgramRepository();
